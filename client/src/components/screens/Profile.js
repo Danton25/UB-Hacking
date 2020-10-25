c02b7410f27e3bc784d72652626294c63eed4b93
Danton25 <neekhilyaadav25@gmail.com>
2020-10-24 23:16:18 -0400

    Defined FE routes

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -3,7 +3,39 @@ import React from 'react';
 
 const Profile = () => {
     return(
-        <h1>Profile screen</h1>
+        <div style={{maxWidth:"550px", margin:"0px auto"}}>
+            <div style={{
+                display:"flex",
+                justifyContent:"space-around",
+                margin:"18px 0px",
+                borderBottom:"1px solid grey"
+            }}>
+                <div>
+                    <img style={{width:"160px",height:"160px",borderRadius:"80px"}}
+                    src="https://images.unsplash.com/photo-1597466765990-64ad1c35dafc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=832&q=80"
+                    alt=""/>
+                </div>
+                <div>
+                    <h4>Danton</h4>
+                    <div style={{display:"flex",justifyContent:"space-between", width:"108%"}}>
+                        <h6>40 posts</h6>
+                        <h6>45 followers</h6>
+                        <h6>45 following</h6>
+                    </div>
+                </div>
+            </div>
+        
+            <div className="gallery">
+                <img className="item" src="https://images.unsplash.com/photo-1544890225-2f3faec4cd60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1250&q=80" alt=""/>
+                <img className="item" src="https://images.unsplash.com/photo-1544890225-2f3faec4cd60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1250&q=80" alt=""/>
+                <img className="item" src="https://images.unsplash.com/photo-1544890225-2f3faec4cd60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1250&q=80" alt=""/>
+                <img className="item" src="https://images.unsplash.com/photo-1544890225-2f3faec4cd60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1250&q=80" alt=""/>
+                <img className="item" src="https://images.unsplash.com/photo-1544890225-2f3faec4cd60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1250&q=80" alt=""/>
+                <img className="item" src="https://images.unsplash.com/photo-1544890225-2f3faec4cd60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1250&q=80" alt=""/>
+                <img className="item" src="https://images.unsplash.com/photo-1544890225-2f3faec4cd60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1250&q=80" alt=""/>
+                <img className="item" src="https://images.unsplash.com/photo-1544890225-2f3faec4cd60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1250&q=80" alt=""/>
+            </div>
+        </div>
     );
 }
 
--- client/src/components/screens/SignIn.js
+++ client/src/components/screens/SignIn.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+
 const SignIn = () => {
     return(
         <div className = "mycard">
             <div className="card auth-card input-field">
                 <h2>TheGram</h2>
-                <input 
-                type = "text"
-                placeholder = "name"
-                />
-
                 <input 
                 type = "text"
                 placeholder = "email"
@@ -21,10 +17,10 @@ const SignIn = () => {
                 placeholder = "password"
                 />
                 <button className="btn waves-effect waves-light #64b5f6 blue lighten-2" >
-                    Sign Up
+                    Login
                 </button>
 
-                <h5 Link to = "/signin">Already have an account</h5>
+                <h5 Link to = "/signup">Don't have an account?</h5>
             </div>
         </div>
     );
