451f5a9fe14bf906d644a90b0c222b218c6050da
Danton25 <neekhilyaadav25@gmail.com>
2020-10-25 05:30:15 -0400

    complted signup route with backend connections

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -8,7 +8,7 @@ const SignIn = () => {
     const[email, setEmail] = useState("")
     const PostData = () =>{
         if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
-            M.toast({html:"Invalid email", classes:"#c62828 red darken-3"})
+            M.toast({html:"invalid email",classes:"#c62828 red darken-3"})
             return
         }
         fetch("/signup",{
